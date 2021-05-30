c19fce3f3293771f2ee7c67b1f4c1400f14b2fbd
Joel Lau <joel.lau@protonmail.com>
2021-05-30 11:54:04 +0800

    added tests for nav component

--- src/app/layout/components/nav/nav.component.spec.ts
+++ src/app/layout/components/nav/nav.component.spec.ts
@@ -1,4 +1,7 @@
+import { CUSTOM_ELEMENTS_SCHEMA, DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { IfScreensizeDirective } from '@shared/directives/if-screensize/if-screensize.directive';
 import { NavComponent } from './nav.component';
 
 describe('NavComponent', () => {
@@ -7,7 +10,8 @@ describe('NavComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [NavComponent],
+      declarations: [NavComponent, IfScreensizeDirective],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
     }).compileComponents();
   });
 
@@ -19,5 +23,37 @@ describe('NavComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it("should have a child 'nav-desktop' component", () => {
+    const debugElement: DebugElement[] = fixture.debugElement.queryAll(
+      By.css('[data-test="nav-desktop"]')
+    );
+
+    expect(debugElement.length).toBeGreaterThanOrEqual(1);
+  });
+
+  it("should have a child 'nav-mobile' component", () => {
+    spyOnProperty(window, 'innerWidth').and.returnValue(9999);
+    window.dispatchEvent(new Event('resize'));
+    fixture.detectChanges();
+
+    const debugElement: DebugElement[] = fixture.debugElement.queryAll(
+      By.css('[data-test="nav-mobile"]')
+    );
+
+    expect(debugElement.length).toBe(0);
+  });
+
+  it("should hide child 'nav-mobile' component", () => {
+    spyOnProperty(window, 'innerWidth').and.returnValue(1);
+    window.dispatchEvent(new Event('resize'));
+    fixture.detectChanges();
+
+    const debugElement: DebugElement[] = fixture.debugElement.queryAll(
+      By.css('[data-test="nav-mobile"]')
+    );
+
+    expect(debugElement.length).toBe(1);
   });
 });
