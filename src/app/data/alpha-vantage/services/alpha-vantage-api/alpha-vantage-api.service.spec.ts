3ebc4ff8c06de3430eb707732171f63ba11fb7db
Joel Lau <joel.lau@protonmail.com>
2021-06-05 17:43:25 +0800

    creatable service

---
+++ src/app/data/alpha-vantage/services/alpha-vantage-api/alpha-vantage-api.service.spec.ts
@@ -0,0 +1,18 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { TestBed } from '@angular/core/testing';
+import { AlphaVantageApiService } from './alpha-vantage-api.service';
+
+describe('AlphaVantageApiService', () => {
+  let service: AlphaVantageApiService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
+    service = TestBed.inject(AlphaVantageApiService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
